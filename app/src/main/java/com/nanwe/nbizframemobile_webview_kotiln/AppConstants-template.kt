5f120554c72b087e7c29dad4f11008f573b4b753
yhj0904 <ujk6073@gmail.com>
2025-06-11 15:27:40 +0900

    :memo: AppContants template ...

---
+++ app/src/main/java/com/nanwe/nbizframemobile_webview_kotiln/AppConstants-template.kt
@@ -0,0 +1,24 @@
+package com.nanwe.nbizframemobile_webview_kotiln
+
+object AppConstants {
+
+    const val APP_ID = "MOBILE"
+
+    const val PUSH_SAVE_TOKEN_URL = "/api/token/register"
+    const val PUSH_REMOVE_TOKEN_URL = "/api/token/delete"
+
+    const val PUSH_CHANNEL_ID = "알림"
+    const val PUSH_CHANNEL_NM = "알림 Name"
+    const val PUSH_CHANNEL_DES = "알림 서비스"
+
+
+    const val APP_URL = "https://localhost:8080"
+
+    const val UID = "uid"
+    const val SVCID = "svcid"
+    const val REASON = "reason"
+    const val RETVAL = "returnvalue"
+
+    const val CODE_SUCCESS = 0      // 처리결과 코드(성공)
+    const val CODE_ERROR = -1       // 처리결과 코드(실패)
+}
